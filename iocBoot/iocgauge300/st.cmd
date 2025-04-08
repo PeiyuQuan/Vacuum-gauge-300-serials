70c7d15c5210b6946c751640f20a7e065041ba4e
Quan, Peiyu <41603034+PeiyuQuan@users.noreply.github.com>
2025-04-07 17:23:52 -0700

    Update st.cmd

--- iocBoot/iocgauge300/st.cmd
+++ iocBoot/iocgauge300/st.cmd
@@ -1,7 +1,5 @@
 #!../../bin/linux-x86_64/gauge300
 
-#- You may have to change gauge300 to something else
-#- everywhere it appears in this file
 
 < envPaths
 
@@ -14,7 +12,6 @@ gauge300_registerRecordDeviceDriver pdbbase
 epicsEnvSet("STREAM_PROTOCOL_PATH","${TOP}/gauge300App/Db")
 epicsEnvSet("PREFIX", "SLAC:Gauge300s:")
 epicsEnvSet("PORT", "serial1")
-epicsEnvSet("M","kjl1:")
 
 ## Load record instances
 #drvAsynSerialPortConfigure("portName","ttyName",priority,noAutoConnect,noProcessEosIn)
@@ -30,18 +27,12 @@ asynSetOption("serial1",0,"parity","none")
 asynSetOption("serial1",0,"clocal","Y")
 asynSetOption("serial1",0,"crtscts","N")
 
-dbLoadRecords("${TOP}/gauge300App/Db/gauge300s.db","P=$(PREFIX),M=$(M),PORT=serial1")
+dbLoadRecords("${TOP}/gauge300App/Db/gauge300s.db","P=$(PREFIX),M=kjl1:,PORT=serial1")
+#dbLoadRecords("${TOP}/gauge300App/Db/gauge300s.db","P=$(PREFIX),M=kjl2:,PORT=serial1")
+#dbLoadRecords("${TOP}/gauge300App/Db/gauge300s.db","P=$(PREFIX),M=kjl3:,PORT=serial1")
 dbLoadRecords ("$(ASYN)/db/asynRecord.db", "P=$(PREFIX), R=asyn1, PORT=serial1, ADDR=0, IMAX=80, OMAX=80")
 dbLoadRecords ("$(AUTOSAVE)/db/save_restoreStatus.db", "P=$(PREFIX)")
 
-#- Set this to see messages from mySub
-#var mySubDebug 1
-
-#- Run this to trace the stages of iocInit
-#traceIocInit
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
-
-## Start any sequence programs
-#seq sncExample, "user=quan"
